e56a264ba2392c974a55fd20b3c81066e69cbae2
Siva <sivaraaman.s@gmail.com>
2021-05-23 22:53:58 +0800

    intial code commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# node-ip-limit
+
+
+# ![Simple Node App]
+
+> It's just simple node app with ip limiting function.
+
+
+## Getting started
+
+Before start please read the following:
+
+1. Download code to your machine
+2. Install dependencies
+
+```bash
+# install dependencies
+> npm install 
+```
+3. Start the application
+
+```bash
+# To Start the application
+> npm run start
+```
+3. Access the below url in your brwser
+[node-app](http://localhost:3000) 
+
+## To know
+
+There have .env file which all config's are placed
+
+- Update .env file if you want to change server port, req limit and time interval
+- Interval given in mitutes
+- Note if you updated the config after the server start you need to restart the server
+
+
+
---
+++ src/app.ts
@@ -0,0 +1,72 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from 'cors';
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+let ipHitsMap: any = {};
+const limit: any = process.env.REQ_LIMIT;
+const minutes: number = 1000 * 60;
+const ipInterval: any = process.env.RESET_INTERVAL;
+const resetInterval: any = minutes * ipInterval;
+
+
+console.log(limit);
+console.log(resetInterval);
+console.log(minutes);
+
+const app = express();
+app.use(cors())
+// app.use(LimitMiddleware.limitByIp());
+
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send(`welcome to CarTrack`)
+} );
+
+// app.post( "/login", ( req, res ) => {
+//     // const check = LimitMiddleware.limitByIp();
+//     // console.log(check);
+//     res.send(`welcome to CarTrack`)
+//     // res.send(check)
+// } );
+
+app.post( "/login", ( req, res, next ) => {
+    const requesterIp = String(req.headers['x-forwarded-for'] || req.connection.remoteAddress || 'unknown');
+    startResetInterval();
+    console.log('requesterIp', requesterIp);
+
+    if (!ipHitsMap[requesterIp]) {
+      ipHitsMap[requesterIp] = 1;
+    } else {
+      ipHitsMap[requesterIp] = ipHitsMap[requesterIp] + 1;
+    }
+
+    if (ipHitsMap[requesterIp] > limit) {
+      const rate = resetInterval/minutes;
+      console.log( `Your ip has exceeded the ${limit} request limit per ${rate} minute(s). Try again in ${rate} minute(s).`)
+      res.send( { message: `Your ip has exceeded the ${limit} request limit per ${rate} minute(s). Try again in ${rate} minute(s).`, timestamp: Date.now()})
+    } else {
+        res.send({ message: `welcome to CarTrack`, timestamp: Date.now()})
+    }
+} );
+
+function startResetInterval(interval = resetInterval) {
+    setInterval(() => resetIpHitsMap(), interval);
+}
+
+function resetIpHitsMap() {
+    ipHitsMap = {};
+}
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
